849b737741d3dda39fe4e6884315fa72ab0cfaa8
Louis Bouchard <louis@ubuntu.com>
2017-09-01 19:55:50 +0200

    [automate] initial version, WIP
    Signed-off-by: Louis Bouchard <louis@ubuntu.com>

---
+++ php-fpm_entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# init.sh script
+
+echo "#################################"
+echo "### Running custom entrypoint ###"
+echo "#################################"
+# Install WordPress
+STOPPED=1
+COUNTER=10
+TIMEOUT=5
+while [ $STOPPED -ne 0 ] && [ $COUNTER -ne 0 ]; do
+    COUNTER=`expr $COUNTER - 1`
+    sleep $TIMEOUT
+    #nc -z mariadb 3306 -w 1
+    /opt/bitnami/php/bin/php -r 'mysqli_connect("mariadb", "wordpress", "my-password", null, 3306) or exit(1);'
+    STOPPED=$?
+    if [ $STOPPED -ne 0 ]; then
+    	echo "mariadb: not started yet"
+    fi
+done
+
+if [ -f /app/wp_automate.php ] ; then
+    /opt/bitnami/php/bin/php /app/wp_automate.php
+fi
+
+/entrypoint.sh php-fpm
