e42ebb4681b0a69a2bfea5e1fd06aee3d687e7ed
Louis Bouchard <louis@ubuntu.com>
2017-09-02 11:48:40 +0200

    [automate] Add missing initialization vars
    Signed-off-by: Louis Bouchard <louis@ubuntu.com>

--- wp_automate.php
+++ wp_automate.php
@@ -18,10 +18,10 @@ if ($db=($GLOBALS["___mysqli_ston"] = mysqli_connect("mariadb", "wordpress", "my
     extract($result, EXTR_SKIP);
 
 
-    ((bool)mysqli_query($db, "USE " . WORDPRESS_DATABASE));
-    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE wp_users SET user_pass = MD5('WORDPRESS_USER_PASSWORD') WHERE wp_users.ID=1 LIMIT 1");
-    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE wp_options SET option_value = 'WORDPRESS_URL' WHERE wp_options.option_name = 'siteurl'");
-    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE wp_options SET option_value = 'WORDPRESS_URL' WHERE wp_options.option_name = 'home'");
+    ((bool)mysqli_query($db, "USE " . wordpress));
+    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE wp_users SET user_pass = MD5('caribou') WHERE wp_users.ID=1 LIMIT 1");
+    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE wp_options SET option_value = 'http://wordpress.example.com' WHERE wp_options.option_name = 'siteurl'");
+    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE wp_options SET option_value = 'http://wordpress.example.com' WHERE wp_options.option_name = 'home'");
 
     ((is_null($___mysqli_res = mysqli_close($db))) ? false : $___mysqli_res);
     echo('Your Wordpress installation is now configured.');
