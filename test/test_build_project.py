6e1f0e9b9eb0a9282de2615f623a1cf93dcdada9
Louis Bouchard <louis@ubuntu.com>
2017-09-04 11:59:54 +0200

    [test] Add tarfile handling test coverage
    Signed-off-by: Louis Bouchard <louis@ubuntu.com>

--- test/test_build_project.py
+++ test/test_build_project.py
@@ -1,5 +1,7 @@
 import unittest
 import build_project
+import sys
+import tarfile
 from unittest.mock import patch, MagicMock
 
 
@@ -12,6 +14,7 @@ class BuildProjectTests(unittest.TestCase):
         self.fake_md5_hash.add_spec('hexdigest')
         self.fake_md5_query = MagicMock()
         self.fake_md5_query.add_spec('read')
+        self.fake_tarfile = MagicMock()
 
     @patch('builtins.open')
     @patch('build_project.hashlib.md5')
@@ -83,3 +86,37 @@ class BuildProjectTests(unittest.TestCase):
         '''
         ret = build_project.get_latest_wp()
         self.assertFalse(ret)
+
+    @patch('build_project.tarfile.open')
+    @patch('build_project.os.path.exists', return_value=True)
+    @patch('build_project.shutil.rmtree')
+    def test_extract_wp_tarball(self, m_rmtree, m_exists, m_tarfile):
+        '''
+        Test tarball cleanup and extraction
+        '''
+        m_tarfile.return_value = self.fake_tarfile
+        ret = build_project.extract_wp_tarball()
+        m_rmtree.assert_called_once_with(self.wp_latest['dir'])
+        m_tarfile.assert_called_once_with(self.wp_latest['file'], 'r')
+        self.fake_tarfile.extractall.assert_called_once_with()
+        self.fake_tarfile.close.assert_called_once_with()
+
+    @patch('build_project.os.path.exists', return_value=True)
+    @patch('build_project.shutil.rmtree', side_effect=PermissionError)
+    def test_extract_wp_tarball_cleanup_error(self, m_rmtree, m_exists):
+        '''
+        Test cleanup exception handling
+        '''
+        ret = build_project.extract_wp_tarball()
+        self.assertFalse(ret)
+
+    @patch('build_project.tarfile.open', side_effect=tarfile.TarError)
+    @patch('build_project.os.path.exists', return_value=False)
+    def test_extract_wp_tarball_tar_exception(self, m_exists, m_tarfile):
+        '''
+        Test tarball cleanup and extraction
+        '''
+        ret = build_project.extract_wp_tarball()
+        self.assertFalse(ret)
+        output = sys.stdout.getvalue().strip()
+        self.assertEquals(output, 'Unable to extract the tarball :')
